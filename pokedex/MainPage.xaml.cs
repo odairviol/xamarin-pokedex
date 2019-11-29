9994bcffcefa191e1a994ef166e4118cf5ed37fb
odair.viol@gmail.com <pos@Mac-mini-de-IFSP.local>
2019-11-29 19:58:12 -0300

    Projeto em pokedex

---
+++ pokedex/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+namespace pokedex
+{
+    public partial class MainPage : ContentPage
+    {
+        internal ObservableCollection<Pokemon> pokemons = new ObservableCollection<Pokemon>
+        {
+            new Pokemon {
+                Imagem = "abra.png",
+                Nome="Abra",
+                Tipo="Psi"
+            },
+            new Pokemon {
+                Imagem = "alakazam.png",
+                Nome="Alakazam",
+                Tipo="Psi"
+            },
+            new Pokemon {
+                Imagem = "blastoise.png",
+                Nome="Blastoise",
+                Tipo="Água"
+            },
+            new Pokemon {
+                Imagem = "Bulbasaur.png",
+                Nome="Bulbasaur",
+                Tipo="Grama"
+            }
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+            listPokemon.ItemsSource = pokemons;
+        }
+
+        private void OnSelection(object sender, SelectedItemChangedEventArgs e)
+        {
+            if(e == null)
+            {
+                return;
+            }
+
+            var detailPage = new DetailPage();
+            detailPage.BindingContext = e.SelectedItem as Pokemon;
+            //listPokemon.SelectedItem = null;
+            Navigation.PushAsync(detailPage);
+        }
+    }
+}
---
+++ pokedex/Pokemon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace pokedex
+{
+    public class Pokemon
+    {
+
+        public Pokemon()
+        {
+        }
+
+        public string Imagem { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+    }
+}
